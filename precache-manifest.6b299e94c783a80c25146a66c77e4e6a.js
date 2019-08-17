98002310861a982c17ef44c6463fc400e01d7c09
Piyawut Chantasrisawat <BZ@192-168-1-163.tpgi.com.au>
2019-08-17 16:37:02 +1000

    Updates

---
+++ precache-manifest.6b299e94c783a80c25146a66c77e4e6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7efe3eff8e97d02802cec04f3ba1a09",
+    "url": "/piyawutcha.github.io/index.html"
+  },
+  {
+    "revision": "ad55b46579e68371ed54",
+    "url": "/piyawutcha.github.io/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1bae4874552c1af1600c",
+    "url": "/piyawutcha.github.io/static/js/2.83750935.chunk.js"
+  },
+  {
+    "revision": "ad55b46579e68371ed54",
+    "url": "/piyawutcha.github.io/static/js/main.e454ec79.chunk.js"
+  },
+  {
+    "revision": "799fdfecc5ed3172727f",
+    "url": "/piyawutcha.github.io/static/js/runtime~main.70fdf511.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/piyawutcha.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
