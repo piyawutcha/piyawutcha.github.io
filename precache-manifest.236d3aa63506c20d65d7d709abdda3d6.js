633b533a863f51c7d7ebef2efb2cd99e674ab8e7
Piyawut Chantasrisawat <BZ@192-168-1-163.tpgi.com.au>
2019-08-17 16:17:50 +1000

    Updates

---
+++ precache-manifest.236d3aa63506c20d65d7d709abdda3d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4440dcb84dbea454125f3bc65d608a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc1b082cfaa6eefee53f",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1bae4874552c1af1600c",
+    "url": "/static/js/2.83750935.chunk.js"
+  },
+  {
+    "revision": "fc1b082cfaa6eefee53f",
+    "url": "/static/js/main.39a2bf01.chunk.js"
+  },
+  {
+    "revision": "bd71bab4267f6b44b519",
+    "url": "/static/js/runtime~main.5bed6346.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
