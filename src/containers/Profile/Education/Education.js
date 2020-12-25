31234bbf82885bfc375154ce69ee3a39fed5970c
Piyawut Chantasrisawat <BZ@Piyawuts-MacBook-Pro-2.local>
2020-12-25 17:05:07 +1100

    Create Education Section
    * Create Education section
    * Add Material UI library

--- src/containers/Profile/Education/Education.js
+++ src/containers/Profile/Education/Education.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Education.css';
+import School from '../../../components/School/School';
+import Typography from '@material-ui/core/Typography';
 
-const education = () => (
-  <div className="education">
-    <h3>Education</h3>
-  </div>
-);
+class Education extends Component {
+  state = {
+    schools : [
+      {
+        abbr : 'MU',
+        name : 'Mahidol University',
+        degree : 'Bachelor of Science, Information and Communication Technology',
+        major : 'Computer Science',
+        period : '2010 - 2013',
+      },
+      {
+        abbr : 'UTS',
+        name : 'University of Techonlogy Sydney',
+        degree : 'Master of Information Technology',
+        major : 'Software Development',
+        period : '2015 - 2017',
+      },
+    ]
+  }
 
-export default education;
+  render() {
+    const schools = this.state.schools.map(school => {
+      return (
+        <School name={school.name} degree={school.degree} major={school.major} period={school.period} abbr={school.abbr} key ={school.abbr}/>
+      )
+    });
+
+    return (
+      <div className="education">
+        <Typography variant="h2">Education</Typography>
+        <div class="school_container">
+          {schools}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Education;
