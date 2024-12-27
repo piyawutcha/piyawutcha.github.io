05718ba3995a391a7630b44f53d418cfea5abd69
piyawutcha <piyawut.cha@gmail.com>
2024-12-27 21:59:56 +1100

    Create Education section

---
+++ src/components/Education/EducationCard.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const EducationCard = (params) => {
+  let education = params.education
+
+  return (
+    <div className="w-full px-14 py-5 border-black border-solid border-[4px] rounded-3xl flex justify-between mb-8">
+      <div className="w-2/12">
+        <img src={education.logo} alt={education.name} className="w-32" />
+      </div>
+      <div className="w-10/12">
+        <div className="font-semibold text-xl">
+          {education.name}
+        </div>
+        <div className="font-light text-lg mt-1">
+          {education.degree}
+        </div>
+        <div className="font-light text-lg mt-1">
+          {education.major}
+        </div>
+        <div className="font-extralight text-gray-700 text-sm mt-1">
+          {education.year}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default EducationCard
